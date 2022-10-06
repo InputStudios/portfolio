b21a1dbd4b65130c4851e8a4d7c6ad2b7a35eb10
Andrey <Hitmanki@yandex.ru>
2022-10-07 00:44:32 +0400

    Message php form fixed

--- form/send.php
+++ form/send.php
@@ -4,20 +4,23 @@
 	$login = $_POST['login'];
 	$email = $_POST['email'];
 	$tel = $_POST['tel'];
+	$message = $_POST['message'];
 
 	// обработаем полученные данные
 	$login = htmlspecialchars($login); // преобразование символов в сущности (мнемоники)
 	$email = htmlspecialchars($email);
 	$tel = htmlspecialchars($tel);
+	$message = htmlspecialchars($message);
 
 	$login = urldecode($login); // декодирование URL
 	$email = urldecode($email);
 	$tel = urldecode($tel);
+	$message = urldecode($message);
 
 	$login = trim($login); // удаление проблемных символов с обеих сторон
 	$email = trim($email);
 	$tel = trim($tel);
-
+	$message = trim($message);
 
 	// отправляем данные на почту
 	if(mail(
@@ -26,6 +29,7 @@
 		"Логин: ".$login."\n".
 		"Email: ".$email."\n".
 		"Телефон: ".$tel,
+		"Сообщение: ".$message,
 		"From: no-reply@mydomain.ru \r\n")
 
 	) {
