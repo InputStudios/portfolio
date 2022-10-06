5a8d70f1c8ed7f5f62c4296bf3c112556615bee0
Andrey <Hitmanki@yandex.ru>
2022-10-06 23:49:43 +0400

    Create PHP form

---
+++ form/send.php
@@ -0,0 +1,38 @@
+<?php
+
+	// получим данные с элементов формы
+	$login = $_POST['login'];
+	$email = $_POST['email'];
+	$tel = $_POST['tel'];
+
+	// обработаем полученные данные
+	$login = htmlspecialchars($login); // преобразование символов в сущности (мнемоники)
+	$email = htmlspecialchars($email);
+	$tel = htmlspecialchars($tel);
+
+	$login = urlcode($login); // декодирование URL
+	$email = urlcode($email);
+	$tel = urlcode($tel);
+
+	$login = trim($login); // удаление проблемных символов с обеих сторон
+	$email = trim($email);
+	$tel = trim($tel);
+
+
+	// отправляем данные на почту
+	if(mail(
+		"verclocker1@gmail.com",
+		"Новое письмо с сайта",
+		"Логин: ".$login."\n".
+		"Email: ".$email."\n".
+		"Телефон: ".$tel,
+		"From: no-reply@mydomain.ru \r\n")
+
+	) {
+		echo ('Письмо успешно отправлено!');
+	}
+
+	else {
+		echo ('Есть ошибки! Проверьте данные...');
+	}
+?>
