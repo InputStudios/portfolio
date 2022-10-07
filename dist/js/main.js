c8b1c8d239df542f5254f82e15910eb6d786354e
Andrey <Hitmanki@yandex.ru>
2022-10-07 12:49:22 +0400

    Fixed select language

--- dist/js/main.js
+++ dist/js/main.js
@@ -243,36 +243,36 @@ themeButton.addEventListener('click', () => {
 })
 
 /*======================== LANGUAGE SWITCHING MODE ========================*/
-// const select = document.querySelector('select');
-// const allLang = ['en', 'ru'];
+const select = document.querySelector('select');
+const allLang = ['en', 'ru'];
 
-// select.addEventListener('change', changeURLLanguage);
+select.addEventListener('change', changeURLLanguage);
 
-// // Redirect url to language
-// function changeURLLanguage() {
-// 	let lang = select.value;
-// 	location.href = window.location.pathname + '#' + lang;
-// 	location.reload();
-// }
+// Redirect url to language
+function changeURLLanguage() {
+let lang = select.value;
+location.href = window.location.pathname + '#' + lang;
+location.reload();
+}
 
-// function changeLanguage() {
-// 	let hash = window.location.hash;
-// 	hash = hash.slice(1);
-// 	console.log(hash);
-// 	if (!allLang.includes(hash)) {
-// 		location.href = window.location.pathname + '#en';
-// 		location.reload();
-// 	}
-// 	select.value = hash;
-// 	//document.querySelector('.nav__item').innerHTML = langArr['nav__item'][hash];
-// 	for (let key in langArr) {
-// 		let elem = document.querySelector('.lng-'+ key);
-// 		if(elem) {
-// 			elem.innerHTML = langArr[key][hash];
-// 		}
-// 	}
-// }
-// changeLanguage();
+function changeLanguage() {
+let hash = window.location.hash;
+hash = hash.slice(1);
+console.log(hash);
+if (!allLang.includes(hash)) {
+	location.href = window.location.pathname + '#en';
+	location.reload();
+}
+select.value = hash;
+//document.querySelector('.nav__item').innerHTML = langArr['nav__item'][hash];
+for (let key in langArr) {
+	let elem = document.querySelector('.lng-'+ key);
+	if(elem) {
+		elem.innerHTML = langArr[key][hash];
+	}
+}
+}
+changeLanguage();
 
 /*==================== CONTACT FORM ====================*/
 const form = document.forms["form"];
